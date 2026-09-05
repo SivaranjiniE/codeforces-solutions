b0d096da7148707817d038daac245c3af4f6ff2d
SivaranjiniE <shivuelumalairr@gmail.com>
2026-09-06 01:12:04 +0530

    Update sync script and gitignore

--- sync.py
+++ sync.py
@@ -8,8 +8,18 @@ response = requests.get(url)
 data = response.json()
 
 submissions = data["result"]
+last_submission = 0
 
+try:
+    with open("last_submission.txt", "r") as file:
+        last_submission = int(file.read())
+
+except FileNotFoundError:
+    pass
 for submission in submissions:
+    if submission["id"] <= last_submission:
+        continue
+
     if submission["verdict"] == "OK":
 
         problem = submission["problem"]
@@ -21,3 +31,8 @@ for submission in submissions:
         print("Rating:", problem.get("rating"))
         print("Language:", submission["programmingLanguage"])
         print("-" * 40)
+if submissions:
+    latest_submission = submissions[0]["id"]
+
+    with open("last_submission.txt", "w") as file:
+        file.write(str(latest_submission))
